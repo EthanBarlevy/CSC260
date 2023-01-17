96817315ef828d7dcfe5cbb6ecd71910605cf582
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2023-01-17 15:18:36 -0700

    added a little bit of notes and started the videogames lab

--- Movies/Controllers/MovieController.cs
+++ Movies/Controllers/MovieController.cs
@@ -29,3 +29,14 @@ namespace Movies.Controllers
 		}
 	}
 }
+
+// DateTime dt = DateTime.Now
+// Model.Count()
+// LoanDate = null
+// Model.LoanDate.HasValue
+// LoanDate.Value.ToShortDateString() "1/1/1111"
+// LoanDate.Value.ToLongDateString() "January 1, 1111"
+// model will store the filename for the image <img src"/images/@Model.ImageName" />
+// Game oneame = lstGames.Find(g => g.ID == ID);
+// lstGames.FindIndex(g => g.ID ==ID);
+// use a form for loaning
